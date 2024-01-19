68f8cac95f8e7abe30cb34b98be51206e29dd589
jglowiszyn <jglowiszyn@wp.pl>
2024-01-19 18:57:10 +0100

    Chapter 4

---
+++ src/Chapter4/WhoAmI.java
@@ -0,0 +1,17 @@
+package Chapter4;
+
+public class WhoAmI {
+    // 1) instance variable, method
+    // 2) return
+    // 3) return, argument
+    // 4) encapsulation
+    // 5) pass by value
+    // 6) instance variables
+    // 7) argument
+    // 8) setter
+    // 9) public
+    // 10) method
+    // 11) setter
+    // 12) getter, setter, public, private
+    // 13) return
+}
